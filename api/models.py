d32e79b74ee8ba7cb05f4e9056707feab5cc39a4
JuanCampuzan0 <juancamdi@unisabana.edu.co>
2025-05-06 17:35:03 -0500

    Actualización de nombre en /authors

--- api/models.py
+++ api/models.py
@@ -3,3 +3,10 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.name} ({self.code})"
