cbca40ae2b92ea32241daf4baff1b422a1a47023
JuanCampuzan0 <juancamdi@unisabana.edu.co>
2025-05-06 17:58:50 -0500

    Actualización de nombre en /authors

--- api/views.py
+++ api/views.py
@@ -1,10 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Author  # Import the Author model
 
 @api_view(['GET'])
 def authors(request):
-    return Response({
-        "Juan David Campuzano Díaz": "338832",
-        "Yuly Dayana Rodriguez": "305314",
-        "Juan Diego Lemus": "243911"
-    })
+    authors = Author.objects.all()  # Query all authors from the database
+    authors_data = {author.name: author.code for author in authors}  # Create a dictionary of authors
+    return Response(authors_data)
