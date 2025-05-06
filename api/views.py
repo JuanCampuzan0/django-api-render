473e51e77a55121ca74788b0c9d817b23a39a947
JuanCampuzan0 <juancamdi@unisabana.edu.co>
2025-05-06 18:07:37 -0500

    Versión final para entrega - autores hardcodeados

--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .models import Author  # Import the Author model
 
 @api_view(['GET'])
 def authors(request):
-    authors = Author.objects.all()  # Query all authors from the database
-    authors_data = {author.name: author.code for author in authors}  # Create a dictionary of authors
-    return Response(authors_data)
+    return Response({
+        "Juan David Campuzano Díaz": "338832",
+        "Yuly Dayana Rodriguez": "305314",
+        "Juan Diego Lemus": "243911"
+    })
